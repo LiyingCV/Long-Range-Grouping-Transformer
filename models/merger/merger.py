43e063edf2c85e8d240a94876b65eb4ba2566124
Liying_CV <46776337+LiyingCV@users.noreply.github.com>
2023-12-24 13:56:50 +0800

    upload merger code

---
+++ models/merger/merger.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Refer UMIFormer <https://github.com/GaryZhu1996/UMIFormer>
+
+
+import torch
+from einops import rearrange
+
+from models.transformer_base.encoder.transformer_block import STMBlock
+
+
+class Merger(torch.nn.Module):
+    def __init__(self, cfg):
+        super(Merger, self).__init__()
+        
+        if torch.distributed.get_rank() == 0:
+            print('Merger: Similar Token Merger (STM)')
+        
+        self.blocks = torch.nn.ModuleList(
+            [STMBlock(dim=cfg.NETWORK.MERGER.TCFORMER.DIM, out_token_len=token_len,
+                     k=cfg.NETWORK.MERGER.TCFORMER.K, num_heads=cfg.NETWORK.MERGER.TCFORMER.NUM_HEAD)
+             for token_len in cfg.NETWORK.MERGER.TCFORMER.OUT_TOKEN_LENS])
+
+    def forward(self, feature):
+        # feature [B, V, P, D]
+        feature = rearrange(feature, 'b v l d -> b (v l) d')  # [B, V*P, D]
+        
+        for blk in self.blocks:
+            feature = blk(feature)
+
+        return feature
