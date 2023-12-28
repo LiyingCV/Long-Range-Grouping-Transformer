4f7a44defb7ae215e79eb585fd3d88befb1cda7d
Liying_CV <46776337+LiyingCV@users.noreply.github.com>
2023-12-28 09:32:25 +0800

    Upload loss file

---
+++ losses/losses.py
@@ -0,0 +1,39 @@
+import torch
+
+
+def make_one_hot(labels, classes):
+    one_hot = torch.FloatTensor(labels.size()[0], classes, labels.size()[2], labels.size()[3]).zero_().to(labels.device)
+    target = one_hot.scatter_(1, labels.data, 1)
+    return target
+
+
+class DiceLoss(torch.nn.Module):
+    """based on https://github.com/hubutui/DiceLoss-PyTorch"""
+
+    def __init__(self, smooth=1, p=2, reduction='mean'):
+        super(DiceLoss, self).__init__()
+        self.smooth = smooth
+        self.p = p
+        self.reduction = reduction
+
+    def forward(self, predict, target):
+        assert predict.shape[0] == target.shape[0], "predict & target batch size don't match"
+        predict = predict.contiguous().view(predict.shape[0], -1)
+        target = target.contiguous().view(target.shape[0], -1)
+
+        num = torch.sum(torch.mul(predict, target), dim=1) + self.smooth
+        den = torch.sum(predict.pow(self.p) + target.pow(self.p), dim=1) + self.smooth
+
+        loss = 1 - num / den
+
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        elif self.reduction == 'none':
+            return loss
+        else:
+            raise Exception('Unexpected reduction {}'.format(self.reduction))
+
+
+
