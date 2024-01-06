3cceab82178e9fb5647228e3c37ac1858ff147d8
Liying_CV <46776337+LiyingCV@users.noreply.github.com>
2024-01-06 10:01:20 +0800

    Fix the bug

--- models/transformer_base/encoder/transformer_block.py
+++ models/transformer_base/encoder/transformer_block.py
@@ -68,8 +68,8 @@ class Block(torch.nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=proj_drop)
 
-    def forward(self, x, view_num=None, overlap=False):
-        attn = self.attn(self.norm1(x), view_num=view_num, overlap=overlap)
+    def forward(self, x, view_num=None, lga=False):
+        attn = self.attn(self.norm1(x), view_num=view_num, lga=lga)
         x = x + self.drop_path(attn)
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
